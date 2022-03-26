b8cc27547f59b0e82541c7cd8eb37d8a98461664
Ar-Ray-code <ray255ar@gmail.com>
2022-03-26 21:18:49 +0900

    update launch.py

--- yolox_ros_py/launch/yolox_s_openvino.launch.py
+++ yolox_ros_py/launch/yolox_s_openvino.launch.py
@@ -4,6 +4,9 @@ from launch.actions import IncludeLaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+from urllib.request import urlretrieve
+import os
+
 def generate_launch_description():
     yolox_ros_share_dir = get_package_share_directory('yolox_ros_py')
 
@@ -13,22 +16,14 @@ def generate_launch_description():
             {"image_size": [640,480]},
         ],
     )
-
-    yolox_ros = launch_ros.actions.Node(
-        package="yolox_ros_py", executable="yolox_ros",
+    yolox_openvino = launch_ros.actions.Node(
+        package="yolox_ros_py", executable="yolox_openvino",output="screen",
         parameters=[
             {"image_size/width": 640},
             {"image_size/height": 480},
-            {"yolox_exp_py" : yolox_ros_share_dir+'/yolox_s.py'},
-            {"device" : 'cpu'},
-            {"fp16" : True},
-            {"fuse" : False},
-            {"legacy" : False},
-            {"trt" : False},
-            {"ckpt" : yolox_ros_share_dir+"/yolox_s.pth"},
+            {"device" : 'CPU'},
+            {"model_path" : yolox_ros_share_dir+"/yolox_s.xml"},
             {"conf" : 0.3},
-            {"threshold" : 0.65},
-            {"resize" : 640},
         ],
     )
 
@@ -38,6 +33,6 @@ def generate_launch_description():
 
     return launch.LaunchDescription([
         webcam,
-        yolox_ros,
+        yolox_openvino,
         # rqt_graph
     ])
