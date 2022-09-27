7d35a0393b9efe9d638a8e3cdd97c9ba857faebe
fateshelled <53618876+fateshelled@users.noreply.github.com>
2022-09-27 22:28:37 +0900

    fix typo

--- yolox_ros_cpp/yolox_ros_cpp/src/yolox_ros_cpp.cpp
+++ yolox_ros_cpp/yolox_ros_cpp/src/yolox_ros_cpp.cpp
@@ -23,7 +23,7 @@ namespace yolox_ros_cpp{
         if(this->model_type_ == "tensorrt"){
             #ifdef ENABLE_TENSORRT
                 RCLCPP_INFO(this->get_logger(), "Model Type is TensorRT");
-                this->yolox_ = std::make_unique<yolox_cpp::YoloXTensorRT>(this->model_path_, this->tenorrt_device_,
+                this->yolox_ = std::make_unique<yolox_cpp::YoloXTensorRT>(this->model_path_, this->tensorrt_device_,
                                                                           this->nms_th_, this->conf_th_, this->model_version_);
             #else
                 RCLCPP_ERROR(this->get_logger(), "yolox_cpp is not built with TensorRT");
